ac2bd008944666f3c144fa51b377d96d78ae041e
eydenVi <eydenv85@gmail.com>
2021-05-28 17:31:27 -0500

    updates powershell script

---
+++ azure-template-permissions.ps1
@@ -0,0 +1,21 @@
+echo ${Env:resourceGroupText}
+Connect-AzureAD -Confirm
+Install-Module -Name Az.ManagedServiceIdentity -Force
+
+$msiName          = ${Env:identityName}
+$msiObjectId      = ${Env:msiObjectId}
+$msiResourceGroup = ${Env:resourceGroupName} 
+
+$GraphAppId  = (Get-AzUserAssignedIdentity -ResourceGroupName $msiResourceGroup -Name $msiName).PrincipalId
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter 'appId eq `$GraphAppId`'
+$PermissionName        = 'Application.ReadWrite.OwnedBy'
+
+echo $msiName
+echo $msiObjectId
+echo $msiResourceGroup
+echo $GraphAppId
+echo $GraphServicePrincipal          
+
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains 'Application'}
+New-AzureAdServiceAppRoleAssignment -ObjectId $msiObjectId -PrincipalId $msiObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
