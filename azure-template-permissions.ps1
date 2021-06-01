68fdcfc784e0b317cbedecddf1d6db36d6524ad1
eydenVi <eydenv85@gmail.com>
2021-06-01 09:37:12 -0500

    installs AzureAD module

--- azure-template-permissions.ps1
+++ azure-template-permissions.ps1
@@ -1,14 +1,12 @@
 # Trusted configuration for PSGallery enable us to install required cmdlets without -Force tag
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module -Name AzureAD -Confirm:$false
 
 echo ${Env:groupName}
 echo ${Env:identityName}
 echo ${Env:msiObjectId}
 echo ${Env:graphAppId}
 echo ${Env:subscription}
-
-
-Install-Module -Name Az.ManagedServiceIdentity -RequiredVersion 0.7.2 -Confirm:$false
 
 echo "----------------------------------------------"
 echo "----------------------------------------------"
@@ -24,7 +22,6 @@ $PermissionName        = 'Application.ReadWrite.OwnedBy'
 
 echo "----------------------------------------------"
 echo "----------------------------------------------"
-
 
 $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains 'Application'}
 New-AzureAdServiceAppRoleAssignment -ObjectId $msiObjectId -PrincipalId $msiObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
