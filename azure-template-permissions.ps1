fd45394e4948d3065ff1f5150aba8291b8c81738
eydenVi <eydenv85@gmail.com>
2021-05-31 19:50:43 -0500

    renames groupName

--- azure-template-permissions.ps1
+++ azure-template-permissions.ps1
@@ -6,8 +6,6 @@ echo ${Env:identityName}
 echo ${Env:msiObjectId}
 echo ${Env:graphAppId}
 echo ${Env:subscription}
-echo ${Env:graphAppId}
-echo ${Env:subscription}
 
 
 Install-Module -Name Az.ManagedServiceIdentity -RequiredVersion 0.7.2 -Confirm:$false
@@ -17,15 +15,16 @@ echo "----------------------------------------------"
 
 $msiName          = ${Env:identityName}
 $msiObjectId      = ${Env:msiObjectId}
-$msiResourceGroup = ${Env:resourceGroupName}
-
-echo "----------------------------------------------"
-echo "----------------------------------------------"
+$msiResourceGroup = ${Env:groupName}
 
 $GraphAppId  = ${Env:graphAppId}
 
 $GraphServicePrincipal = Get-AzureADServicePrincipal -Filter 'appId eq `$GraphAppId`'
 $PermissionName        = 'Application.ReadWrite.OwnedBy'
+
+echo "----------------------------------------------"
+echo "----------------------------------------------"
+
 
 $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains 'Application'}
 New-AzureAdServiceAppRoleAssignment -ObjectId $msiObjectId -PrincipalId $msiObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
