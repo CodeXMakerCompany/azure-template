3490459e9e0b19106c075231091518fa3c0e7919
eydenVi <eydenv85@gmail.com>
2021-06-02 13:25:21 -0500

    adds first version of powershell script integration deployment

---
+++ HyperCubeIntegration/grantIdentityAccess.ps1
@@ -0,0 +1,18 @@
+
+$msiName="HyperIdentity"
+$msiObjectId = Read-Host -Prompt 'Input the HyperIdentity objectId: '
+# Windows Azure Active Directory
+$GraphAppId = "00000002-0000-0000-c000-000000000000"
+$GraphServicePrincipal = Get-AzureADServicePrincipal `
+        -Filter "appId eq '$GraphAppId'"
+
+$PermissionName = "Application.ReadWrite.OwnedBy"
+$AppRole = $GraphServicePrincipal.AppRoles  `
+       | Where-Object {$_.Value -eq $PermissionName `
+       -and $_.AllowedMemberTypes -contains "Application"}
+
+New-AzureAdServiceAppRoleAssignment `
+    -ObjectId $msiObjectId `
+    -PrincipalId $msiObjectId  `
+    -ResourceId $GraphServicePrincipal.ObjectId `
+    -Id $AppRole.Id
